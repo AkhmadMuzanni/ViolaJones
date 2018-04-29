a14d39bb21d02c2dd1fa624b2f6d65eba90b54c0
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2018-04-29 11:14:04 +0700

    add data image and edit resizeImg

--- resizeImg.py
+++ resizeImg.py
@@ -10,20 +10,26 @@ from PIL import Image
 
 #ubah berdasarkan width
 def ubahWidth():
-    basewidth = 100
-    img = Image.open('bahan/download.jpg')
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
+    basewidth = 20
+    img = Image.open('bahan/1.jpg')
+    #wpercent = (basewidth / float(img.size[0]))
+    #hsize = int((float(img.size[1]) * float(wpercent)))
+    hsize = 40
     img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
-    img.save('resized_image.jpg')
+    txtHeight = str(hsize)
+    txtWidth = str(basewidth)
+    img.save('resized_image'+txtHeight+'x'+txtWidth+'.jpg')
 
 #ubah berdasarkan height
 def ubahHeight():
     baseheight = 40
     img = Image.open('bahan/1.jpg')
-    hpercent = (baseheight / float(img.size[1]))
-    wsize = int((float(img.size[0]) * float(hpercent)))
+    #hpercent = (baseheight / float(img.size[1]))
+    #wsize = int((float(img.size[0]) * float(hpercent)))
+    wsize = 20
     imgUbah = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
-    imgUbah.save('ubah.jpg')
+    txtHeight = str(baseheight)
+    txtWidth = str(wsize)
+    imgUbah.save('resized_image'+txtHeight+'x'+txtWidth+'.jpg')
 
 ubahHeight();
